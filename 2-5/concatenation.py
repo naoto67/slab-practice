1aaa8d3df0eb501b53d7a2b1d8611e36ffa74da8
naoto67 <uver.com67@gmail.com>
2019-04-02 13:17:53 +0900

    complete 2-5

---
+++ 2-5/concatenation.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img_1 = cv2.imread('./001_001.jpg')
+img_2 = cv2.imread('./002_001.jpg')
+img_3 = cv2.imread('./003_001.jpg')
+img_4 = cv2.imread('./004_001.jpg')
+img_5 = cv2.imread('./005_001.jpg')
+
+images = cv2.vconcat([img_1, img_2, img_3, img_4, img_5])
+
+cv2.imwrite('images.jpg', images)
