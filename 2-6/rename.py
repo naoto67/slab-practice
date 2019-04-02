dd3a26c7a62ead8a55b5759c1e01c64e43cd412e
naoto67 <uver.com67@gmail.com>
2019-04-02 14:06:37 +0900

    complete 2-6

---
+++ 2-6/rename.py
@@ -0,0 +1,24 @@
+import sys
+import ipdb
+
+args = sys.argv
+
+if len(args) != 4:
+    print("arguments must be 4 pieces")
+
+file_list_name = args[1]
+prefix = args[2]
+outfile_name = args[3]
+
+filelist = open(file_list_name, "r")
+outfile = open(outfile_name, "w")
+
+lines = filelist.readlines()
+
+for i, line in enumerate(lines):
+    serial = '_{:0>3}_'.format(i)
+    new_filename = prefix + serial + line.split('_')[-1]
+    outfile.write(new_filename)
+
+filelist.close()
+outfile.close()
