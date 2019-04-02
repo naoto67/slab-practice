f7f9ccf6390fd2f5f1a5669dc57a9910bc30dfa1
naoto67 <uver.com67@gmail.com>
2019-04-02 19:06:13 +0900

    update

--- 2-2/txt2csv.py
+++ 2-2/txt2csv.py
@@ -1,11 +1,21 @@
 import os
 import sys
+import ipdb
+import shutil
 
-filename = sys.argv[1]
-
-if not os.path.isfile(filename):
+old_filename = sys.argv[1]
+if not os.path.isfile(old_filename):
   exit
 
-new_filename = filename.split('.')[0] + '.csv'
+new_filename = old_filename.split('.')[0] + '.csv'
 
-os.rename(filename, new_filename)
+old_file = open(old_filename, "r")
+new_file = open(new_filename, "w")
+
+lines = old_file.readlines()
+
+for line in lines:
+    new_file.write(line.replace(' ', ','))
+
+old_file.close()
+new_file.close()
