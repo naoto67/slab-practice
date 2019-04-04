145e50bd3ba3742691f9611eb582f88ac1355b27
naoto67 <uver.com67@gmail.com>
2019-04-04 11:25:53 +0900

    add error handling

--- 2-5/concatenation.py
+++ 2-5/concatenation.py
@@ -1,12 +1,14 @@
 import cv2
-import numpy as np
 
-img_1 = cv2.imread('./001_001.jpg')
-img_2 = cv2.imread('./002_001.jpg')
-img_3 = cv2.imread('./003_001.jpg')
-img_4 = cv2.imread('./004_001.jpg')
-img_5 = cv2.imread('./005_001.jpg')
+try:
+    img_1 = cv2.imread('./001_001.jpg')
+    img_2 = cv2.imread('./002_001.jpg')
+    img_3 = cv2.imread('./003_001.jpg')
+    img_4 = cv2.imread('./004_001.jpg')
+    img_5 = cv2.imread('./005_001.jpg')
 
-images = cv2.vconcat([img_1, img_2, img_3, img_4, img_5])
+    images = cv2.vconcat([img_1, img_2, img_3, img_4, img_5])
 
-cv2.imwrite('images.jpg', images)
+    cv2.imwrite('images.jpg', images)
+except Exception as e:
+    print(e)
