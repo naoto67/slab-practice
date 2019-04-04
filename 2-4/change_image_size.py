f000b7b50581ca72fd6d5d24a0b27b7f873ee6a4
naoto67 <uver.com67@gmail.com>
2019-04-04 11:25:09 +0900

    add error handling

--- 2-4/change_image_size.py
+++ 2-4/change_image_size.py
@@ -5,12 +5,16 @@ args = sys.argv
 
 if len(args) != 4:
     print("引数が足りません")
-    exit
+    sys.exit(1)
 
 image_name = args[1]
 width = args[2]
 height = args[3]
 
-image = Image.open(image_name)
-resized_image = image.resize((int(width), int(height)))
-resized_image.save(image.filename)
+try:
+    image = Image.open(image_name)
+    resized_image = image.resize((int(width), int(height)))
+    resized_image.save(image.filename)
+except Exception as e:
+    print(e)
+
