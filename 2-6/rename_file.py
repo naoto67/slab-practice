c309cfde4d0b459160086b229415a0712637e32e
naoto67 <uver.com67@gmail.com>
2019-04-09 16:28:55 +0900

    update rename file

--- 2-6/rename_file.py
+++ 2-6/rename_file.py
@@ -11,15 +11,17 @@ file_list_name = args[1]
 prefix = args[2]
 outfile_name = args[3]
 
-filelist = codecs.open(file_list_name, "r", 'shift_jis')
-outfile = open(outfile_name, "w")
+try:
+    filelist = codecs.open(file_list_name, "r", 'shift_jis')
+    outfile = open(outfile_name, "w")
 
-lines = filelist.readlines()
+    lines = filelist.readlines()
 
-for i, line in enumerate(lines):
-    serial = '_{:0>3}_'.format(i)
-    new_filename = prefix + serial + line.split('_')[-1]
-    outfile.write(new_filename)
-
-filelist.close()
-outfile.close()
+    for i, line in enumerate(lines):
+        serial_code = '_{:0>3}_'.format(i)
+        new_filename = prefix + serial_code + line.split('_')[-1]
+        outfile.write(new_filename)
+    filelist.close()
+    outfile.close()
+except Exception as e:
+    print(e)
